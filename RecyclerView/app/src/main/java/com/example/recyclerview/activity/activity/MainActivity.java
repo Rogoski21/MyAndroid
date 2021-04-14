d9c57086f992091b17e87720192a56fcea4726bb
Pedro Rogoski <pedro.rogoski@getnet.com.br>
2021-04-14 15:58:24 -0300

    Continuação do recycler

--- RecyclerView/app/src/main/java/com/example/recyclerview/activity/activity/MainActivity.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/activity/activity/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.recyclerview.activity.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.example.recyclerview.R;
+import com.example.recyclerview.activity.adapter.Adapter;
+import com.example.recyclerview.activity.model.Filme;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private List<Filme> listaFilmes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +27,31 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
 
-        //configurar adapter
+        //Lista de filmes
+        this.criarFilmes();
 
+        //configurar adapter
+        Adapter adapter = new Adapter(listaFilmes);
 
         //configurar recyclerview
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);//configuramos layoumanager
         recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    public void criarFilmes() {
+        //vai gerar os filmes
+        Filme filme = new Filme("Homem Aranha - De volta ao lar", "Ação", "2017");
+        this.listaFilmes.add(filme);
+        filme = new Filme("Homens de preto", "Ação","2010");
+        this.listaFilmes.add(filme);
+        filme = new Filme("A espera de um milagre", "Drama","1999");
+        this.listaFilmes.add(filme);
+        filme = new Filme("Resgate do soldado Ryan", "Ação","1998");
+        this.listaFilmes.add(filme);
 
     }
 
--- RecyclerView/app/src/main/java/com/example/recyclerview/activity/adapter/Adapter.java
+++ RecyclerView/app/src/main/java/com/example/recyclerview/activity/adapter/Adapter.java
@@ -1,5 +1,6 @@
 package com.example.recyclerview.activity.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,35 +8,58 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.recyclerview.R;
+import com.example.recyclerview.activity.model.Filme;
+
+import java.util.List;
+
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
+    private List<Filme> listaFilmes;
+
+    public Adapter(List<Filme> lista) {
+        this.listaFilmes = lista;
+    }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // cria as nossas visualizaçoes
 
-        return null;
+        View itemLista = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.adapter_lista, parent, false);
+
+        return new MyViewHolder(itemLista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        //exibição dos itens
+        Filme filme = listaFilmes.get(position);
+        holder.titulo.setText(filme.getTituloFilme());
+        holder.genero.setText(filme.getGenero());
+        holder.ano.setText(filme.getAno());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        //aqui ele sabe que vai exibir 5 itens
+        return listaFilmes.size();
     }
 
-    //classe para criar os itens
+
     public class MyViewHolder extends RecyclerView.ViewHolder {
+        //classe para criar os itens
         TextView titulo;
         TextView ano;
         TextView genero;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            titulo = itemView.findViewById(R.id.textTitulo);
+            ano = itemView.findViewById(R.id.textAno);
+            genero = itemView.findViewById(R.id.textGenero);
         }
     }
 }
